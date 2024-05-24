2eecf572f534702933e42cc7403f9b4c8c347f5e
WLOADeveloper <leo_071998@hotmail.com>
2024-05-24 16:13:43 -0500

    actualizacion de archivos
    juego pia

---
+++ FalcoGame/scrips/Flag.gd
@@ -0,0 +1,13 @@
+#extends Area2D
+
+#@onready var main = $Flaganimated
+#func _ready():
+	#anim.play("idle")
+
+#func _on_body_entered(body):
+	#if body.is_in_group("player"):
+		#print("Flag 1")
+		#anim.play("tap")
+		#await (anim.animation_finished)
+		#queue_free()
+		#pass
